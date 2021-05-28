0e7ffa5df3231be423d2dfbc3b2d77599c034897
Alona-Suprun <a.alonka.l@gmail.com>
2021-05-28 03:27:38 +0300

    changes [] to {}

--- task-03.js
+++ task-03.js
@@ -9,14 +9,15 @@ const makeTransaction = transaction => {
       const canProcess = Math.random() > 0.3;
 
       if (canProcess) {
-        resolve([transaction.id, delay]);
+        const id = transaction.id;
+        resolve({ id, delay });
       }
       reject(transaction.id);
     }, delay);
   });
 };
 
-const logSuccess = ([id, time]) => {
+const logSuccess = ({ id, time }) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
